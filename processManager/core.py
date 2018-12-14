d9b4c4b9d2e8b9b0de62434897e7d2aca07d91eb
luo3300612 <591486669@qq.com>
2018-12-14 08:29:52 +0800

    update

--- processManager/core.py
+++ processManager/core.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Column, String, create_engine, Integer, Date, ForeignKey
+from sqlalchemy import Column, String, create_engine, Integer, Date, ForeignKey, Boolean
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import IntegrityError
-from datetime import date
+from datetime import date, timedelta
+from functools import singledispatch, reduce
+from operator import add
 
 Base = declarative_base()
 engine = create_engine('mysql+mysqlconnector://root:12345@localhost:3306/processManager')
@@ -22,6 +24,8 @@ class Affair(Base):
     end_day = Column(Date)  # 完成日期
 
     description = Column(String(100))  # 项目描述
+
+    is_completed = Column(Boolean, default=False)  # 是否完成
 
     # 关系
     records = relationship("Record", backref="affair")
@@ -61,31 +65,59 @@ def wrap_up(name):
     """
     affair = session.query(Affair).filter(Affair.name == name).first()
     affair.end_day = date.today()
+    affair.is_completed = True
     session.commit()
+    print(f"{affair.name} is completed")
 
 
 def record(name, workload, thoughts=""):
     """
     工作记录
     """
-    rec = Record(workload=workload, thoughts=thoughts)
-    affair = session.query(Affair).filter(Affair.name == name).first()
+    affair = session.query(Affair).filter(Affair.name == name).first()  # TODO
     if not affair:
         return
-    rec.affair = affair
+    rec = session.query(Record).filter(Record.affair_id == affair.id).filter(Record.date == date.today()).first()
+    if rec:
+        rec.workload += workload
+    else:
+        rec = Record(workload=workload, thoughts=thoughts)
+        rec.affair = affair
 
     affair.process_status += workload
     session.commit()
 
 
-def query(name):
+@singledispatch
+def query_affair(obj):
+    raise NotImplementedError
+
+
+@query_affair.register(str)
+def _(name):
     """
     根据姓名查询
     """
     affair = session.query(Affair).filter(Affair.name == name).first()
     if not affair:
-        return "Not Found!"
+        return False
     return [affair]
+
+
+@query_affair.register(int)
+def _(id):
+    """
+    根据id查询
+    """
+    affair = session.query(Affair).filter(Affair.id == id).first()
+    if not affair:
+        return False
+    return [affair]
+
+
+def query_record(affair):
+    records = session.query(Record).filter(Record.affair_id == affair.id).all()
+    return records
 
 
 def query_all():
@@ -94,7 +126,7 @@ def query_all():
     """
     affairs = session.query(Affair).all()
     if not affairs:
-        return "Not Found!"
+        return False
     return affairs
 
 
@@ -104,13 +136,65 @@ def display(affairs):
     """
     for affair in affairs:
         try:
-            div = affair.process_status / affair.workload
+            div = (affair.process_status + affair.start_from) / affair.workload
             print(
-                f"""<Project:{affair.name},process status:[{int(
+                f"""[{affair.id}]<{affair.description.upper()}:{affair.name},process status:[{int(
                     div * 20) * '#'}{(20 - int(
-                    div * 20)) * '-'}],{div * 100:.2f}%,{affair.process_status}/{affair.workload}>""")
+                    div * 20)) * '-'}],{div * 100:.2f}%,{affair.process_status + affair.start_from}/{affair.workload}>""")
         except ZeroDivisionError as e:
             print(e)
+
+
+def show_all():
+    result = query_all()
+    if result:
+        display(result)
+    else:
+        print("Empty")
+
+
+def show(obj):
+    result = query_affair(obj)
+    if result:
+        display(result)
+    else:
+        print("Not Found")
+
+
+def pred(affair):  # TODO refactor
+    records = query_record(affair)
+    if not records:
+        print("No records")
+        return
+    num_of_work_day = len(records)
+    total_workload = reduce(add, (rec.workload for rec in records))
+
+    period = (affair.start_day - date.today()).days
+    if period == 0:
+        period = 1
+
+    average_workload_day = total_workload / period
+    average_workload_workday = total_workload / num_of_work_day
+
+    expect_workday_salty_fish = int((affair.workload - affair.process_status) / average_workload_day)
+    expect_workday_work_everyday = int((affair.workload - affair.process_status) / average_workload_workday)
+
+    today = date.today()
+    expect_end_day_work_salty_fish = today + timedelta(days=expect_workday_salty_fish)
+    expect_end_day_work_everyday = today + timedelta(days=expect_workday_work_everyday)
+
+    return expect_end_day_work_salty_fish, expect_end_day_work_everyday
+
+
+def monitor(id):
+    affair = session.query(Affair).filter(Affair.id == id).first()  # TODO
+    if not affair:
+        print("Not Found")
+        return
+    fish, dog = pred(affair)
+    print(f"You will complete {affair.name} on:")
+    print(fish.strftime("%Y-%m-%d"), "If you work like a salty fish")
+    print(dog.strftime("%Y-%m-%d"), "If you work like a dog")
 
 
 # display(query("C"))
@@ -125,3 +209,13 @@ def display(affairs):
 #
 # record("A", 5)
 # wrap_up("B")
+
+
+# register(name="计算机视觉——算法与应用", workload=565, description="book", start_from=205)
+# register(name="学习OpenCV", workload=492, description="book", start_from=0)
+# record(name="计算机视觉——算法与应用", workload=236 - 205)
+# record(name="学习OpenCV", workload=25)
+
+#
+#
+#
